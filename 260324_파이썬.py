ab8a23f5d4304ae068231ad60724e2a8e96ed7bb
지윤 <popchoi0504@gmail.com>
2026-03-24 16:04:44 +0900

    제목수정,학교에서 배운걸로수정

---
+++ 260324_파이썬.py
@@ -0,0 +1,15 @@
+# input()
+# print("입력성공")
+
+# a = int(input("숫자를 입력하세요: "))
+# print(a)
+
+# user_name = input("이름을 입력하세요: ")
+# #print("안녕하세요, " + user_name + "님!")
+# print(f"안녕하세요,{user_name}님!")
+
+a=int(input("숫자를 입력하세요: "))
+if a >= 10:
+    print("a는 10보다 크거나 같습니다.")
+else:
+    print("a는 10보다 작습니다.")
